5d7eed253fded24d77827a7b2ecc8f9fca6e6ed8
Dani Vijay <danimvijay@gmail.com>
2018-01-22 22:28:14 +0530

    class

---
+++ class.js
@@ -0,0 +1,46 @@
+class Person {
+  constructor (name, age) {
+    this.name = name,
+    this.age = age
+  }
+  sentence() {
+    console.log(`${this.name} is ${this.age} years old`) 
+  }
+}
+
+class Employee extends Person {
+  constructor (name, age, job, years) {
+    super(name, age)
+    this.job = job,
+    this.years = years
+  }
+  quit() {
+    console.log('I quit my job')
+    this.years = 0
+  }
+}
+
+
+// old
+
+// var Person = function (name, age) {
+//   this.name = name
+//   this.age = age
+// }
+
+// Person.prototype.sentence = function () {
+//   console.log(`${this.name} is ${this.age} years old`) 
+// }
+
+var dani = new Employee('dani', 24, 'programmer', 2)
+var don = new Person('don', 28)
+
+dani
+
+dani.sentence()
+don.sentence()
+
+dani.quit()
+dani
+
+
